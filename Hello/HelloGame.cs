95a4a9e68d9fde536a4abce82ff0cc40d67e1b60
chajus1 <chajus1@googlemail.com>
2023-06-02 02:31:19 +0100

    filled

---
+++ Hello/HelloGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal static class HelloGame
+    {
+        public const int MAXIMUM = 10;
+        static Random random = new Random(10);
+        static private int currentNumber = random.Next(1, 10);
+        static private int nextNumber = random.Next(1, 10);
+        static private int pot = 10;
+
+        internal static int GetPot()
+        {
+            int newPot = pot;
+            return newPot;
+        }
+
+        internal static void Guess(bool higher)
+        {
+            if ((nextNumber >= currentNumber && higher) || (nextNumber <= currentNumber && !higher))
+            {
+                Console.WriteLine("\nYou guessed right!");
+                pot++;
+            
+            } else
+            {
+                Console.WriteLine("\nBad luck, you guessed wrong.");
+                pot--;
+            }
+
+            currentNumber = nextNumber;
+            nextNumber = random.Next(1, 10);
+            Console.WriteLine($"\nThe current number is {currentNumber}.");
+        }
+
+        internal static void Hint()
+        {
+            float half = (float)nextNumber / 2;
+            Console.WriteLine($"\nThe current number is {currentNumber}, the next is at least {half}.");
+            pot--;
+        }
+    }
+}
--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,7 +10,20 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine
+            Console.WriteLine("Welcome to Hello.");
+            Console.WriteLine($"Guess numbers between 1 and {HelloGame.MAXIMUM}.");
+            //HelloGame.Hint();
+            while (HelloGame.GetPot() > 0)
+            {
+                Console.WriteLine("\nPress h for a higher, l for lower, ? to buy a hint,");
+                Console.WriteLine($"or any other key to quit with {HelloGame.GetPot().ToString("c")}.");
+                char key = Console.ReadKey().KeyChar;
+                if (key == 'h') HelloGame.Guess(true);
+                else if (key == 'l') HelloGame.Guess(false);
+                else if (key == '?') HelloGame.Hint();
+                else return;
+            }
+            Console.WriteLine("The is empty. Bye!");
         }
     }
 }
